99667ad6c52095d5bd0eb8b9f0cb8df076da1b99
Yawri Carr <yawri.carr@tum.de>
2025-05-21 19:05:29 +0200

    Fix code style to pass CI checks

--- chroma_db/chroma_setup.py
+++ chroma_db/chroma_setup.py
@@ -7,16 +7,12 @@ client = chromadb.PersistentClient(path="./chroma_storage")
 collection = client.get_or_create_collection(name="research-papers")
 
 collection.add(
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about beer"
-    ],
-    ids=["id1", "id2"]
+    documents=["This is a document about pineapple", "This is a document about beer"],
+    ids=["id1", "id2"],
 )
 
 results = collection.query(
-    query_texts=["This is a query document about munich"], # Chroma will embed this
-    n_results=2 # how many results to return
+    query_texts=["This is a query document about munich"],  # Chroma will embed this
+    n_results=2,  # how many results to return
 )
 print(results)
-
