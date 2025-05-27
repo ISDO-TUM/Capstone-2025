843d6b3c83a383ad351c48d1f220f96a4be1c7a1
sam-rez-cardin <hesam.rezaei@tum.de>
2025-05-27 19:43:05 +0200

    Get_embedding function

---
+++ llm/Embeddings.py
@@ -0,0 +1,7 @@
+from openai import OpenAI
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def get_embedding(text, model="text-embedding-3-small"):
+    text = text.replace("\n", " ")
+    return client.embeddings.create(input = [text], model=model).data[0].embedding
