13a33115d5a3be17eaed4f312894c20edd02bb78
sam-rez-cardin <hesam.rezaei@tum.de>
2025-05-29 11:57:51 +0200

    added Embeddings

--- llm/Embeddings.py
+++ llm/Embeddings.py
@@ -4,7 +4,6 @@ from llm.LLMDefinition import OPENAI_API_KEY
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-
 def embed_string(text, model="text-embedding-3-small"):
     text = text.replace("\n", " ")
     return client.embeddings.create(input=[text], model=model).data[0].embedding
