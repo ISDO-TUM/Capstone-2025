b7e670acd4b7e99e4cad143fd873444402a8d247
AndresDChB <94127440+AndresDChB@users.noreply.github.com>
2025-07-20 16:54:45 +0200

    Removed unnecessary files, updated todos

--- init.sql
+++ init.sql
@@ -39,10 +39,3 @@ CREATE TABLE IF NOT EXISTS public.paperprojects_table (
     FOREIGN KEY (project_id) REFERENCES projects_table(project_id) ON DELETE CASCADE,
     FOREIGN KEY (paper_hash) REFERENCES papers_table(paper_hash) ON DELETE CASCADE
 );
-
--- Clear all data from the table on every startup.
-TRUNCATE TABLE
-    public.paperprojects_table,
-    public.papers_table,
-    public.projects_table
-CASCADE;
