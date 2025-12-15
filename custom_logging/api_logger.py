f8a356faacc3da90b5b91fcbd8ed301f78abec47
subhash <go56yaj@mytum.de>
2025-12-15 21:11:06 +0100

    Implemented API_LOGGER

--- custom_logging/api_logger.py
+++ custom_logging/api_logger.py
@@ -14,4 +14,41 @@ class APILogger(StructuredLogger):
             },
         )
 
-    # ========== TODO ==========
+    def request_error(
+        self, method: str, path: str, error_message: str, status_code: int
+    ):
+        self.error(
+            "API request error",
+            metadata={
+                "method": method,
+                "path": path,
+                "error_message": error_message,
+                "status_code": status_code,
+            },
+        )
+
+    def request_success(self, method: str, path: str, status_code: int):
+        self.info(
+            "API request successful",
+            metadata={
+                "method": method,
+                "path": path,
+                "status_code": status_code,
+            },
+        )
+
+    def request_warning(self, method: str, path: str, warning_message: str):
+        self.warning(
+            "API request warning",
+            metadata={
+                "method": method,
+                "path": path,
+                "warning_message": warning_message,
+            },
+        )
+
+    def request_info(self, method: str, path: str, info_message: str):
+        self.info(
+            "API request info",
+            metadata={"method": method, "path": path, "info_message": info_message},
+        )
